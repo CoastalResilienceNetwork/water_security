5cf45e46c3c574a9b18685094fea1290562cfebf
Casey Schneebeck <cschneebeck@tnc.org>
2017-05-01 08:47:00 -0600

    remove jquery and ui

--- js/esriapi.js
+++ js/esriapi.js
@@ -1,9 +1,9 @@
 define([
 	"esri/layers/ArcGISDynamicMapServiceLayer", "esri/geometry/Extent", "esri/SpatialReference", "esri/tasks/query" ,"esri/tasks/QueryTask", "dojo/_base/declare", "esri/layers/FeatureLayer", 
-	"esri/symbols/SimpleLineSymbol", "esri/symbols/SimpleFillSymbol", "esri/graphic", "dojo/_base/Color", "dojo/_base/lang", "dojo/on", "jquery", './jquery-ui-1.11.2/jquery-ui', "esri/tasks/RelationshipQuery", "esri/graphicsUtils",
+	"esri/symbols/SimpleLineSymbol", "esri/symbols/SimpleFillSymbol", "esri/graphic", "dojo/_base/Color", "dojo/_base/lang", "dojo/on", "esri/tasks/RelationshipQuery", "esri/graphicsUtils",
 ],
 function ( 	ArcGISDynamicMapServiceLayer, Extent, SpatialReference, Query, QueryTask, declare, FeatureLayer, 
-			SimpleLineSymbol, SimpleFillSymbol, Graphic, Color, lang, on, $, ui, RelationshipQuery, graphicsUtils) {
+			SimpleLineSymbol, SimpleFillSymbol, Graphic, Color, lang, on, RelationshipQuery, graphicsUtils) {
         "use strict";
 
         return declare(null, {
